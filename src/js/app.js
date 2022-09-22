55fc8821cc7146c2f6e7606665eb2729c28077f1
Roman <romanchebotarenko@gmail.com>
2022-09-22 14:52:01 +0300

    321

--- src/js/app.js
+++ src/js/app.js
@@ -10,107 +10,116 @@ import { animateOnScroll } from './modules/animate-on-scroll.js'
 import { search } from './modules/search.js'
 import { md, lg, sm } from './modules/breakpoints.js'
 
-const JSparticleEl = document.getElementById('banner__particles')
-particlesJS(
-  'banner__particles',
-
-  {
-    particles: {
-      number: {
-        value: 8,
-        density: {
-          enable: true,
-          value_area: 400
-        }
-      },
-      color: {
-        value: '#348ACC'
-      },
-      shape: {
-        type: 'circle',
-        stroke: {
-          width: 0,
-          color: '#348ACC'
-        },
-        polygon: {
-          nb_sides: 5
-        }
-      },
-      opacity: {
-        value: 1,
-        random: false,
-        anim: {
-          enable: false,
-          speed: 1,
-          opacity_min: 0.1,
-          sync: false
-        }
-      },
-      size: {
-        value: 8,
-        random: true,
-        anim: {
-          enable: true,
-          speed: 1,
-          size_min: 0.25,
-          sync: false
-        }
-      },
-      line_linked: {
+particlesJS('banner__particles', {
+  particles: {
+    number: {
+      value: 8,
+      density: {
         enable: true,
+        value_area: 400
+      }
+    },
+    color: {
+      value: '#348ACC'
+    },
+    shape: {
+      type: 'circle',
+      stroke: {
+        width: 0,
+        color: '#348ACC'
+      },
+      polygon: {
+        nb_sides: 5
+      },
+      image: {
+        src: '',
+        width: 100,
+        height: 100
+      }
+    },
+    opacity: {
+      value: 1,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false
+      }
+    },
+    size: {
+      value: 5,
+      random: true,
+      anim: {
+        enable: true,
+        speed: 1,
+        size_min: 0.1,
+        sync: false
+      }
+    },
+    line_linked: {
+      enable: true,
+      distance: 250,
+      color: '#348ACC',
+      opacity: 1,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 3,
+      direction: 'none',
+      random: true,
+      straight: false,
+      out_mode: 'bounce',
+      bounce: false,
+      attract: {
+        enable: true,
+        rotateX: 600,
+        rotateY: 1200
+      }
+    }
+  },
+  interactivity: {
+    detect_on: 'canvas',
+    events: {
+      onhover: {
+        enable: true,
+        mode: 'repulse'
+      },
+      onclick: {
+        enable: false,
+        mode: 'push'
+      },
+      resize: true
+    },
+    modes: {
+      grab: {
         distance: 300,
-        color: '#348ACC',
+        line_linked: {
+          opacity: 1
+        }
+      },
+      bubble: {
+        distance: 300,
+        size: 9,
+        duration: 10,
         opacity: 1,
-        width: 0
+        speed: 1
       },
-      move: {
-        enable: true,
-        speed: 2,
-        direction: 'none',
-        random: false,
-        straight: false,
-        out_mode: 'bounce',
-        bounce: false,
-        attract: {
-          enable: false,
-          rotateX: 800,
-          rotateY: 1200
-        }
-      }
-    },
-    interactivity: {
-      detect_on: 'canvas',
-      events: {
-        onhover: {
-          enable: true,
-          mode: 'repulse'
-        },
-
-        resize: true
+      repulse: {
+        distance: 400,
+        duration: 0.1
       },
-      modes: {
-        repulse: {
-          distance: 250,
-          duration: 0.4
-        }
+      push: {
+        particles_nb: 4
+      },
+      remove: {
+        particles_nb: 2
       }
-    },
-    retina_detect: true
-  }
-)
-
-/* const obsParticles = new IntersectionObserver(entries =>
-  entries.forEach(entry => {
-    if (entry.isIntersecting) return pJSDom[0].pJS.fn.vendors.start()
-
-    cancelRequestAnimFrame(pJSDom[0].pJS.fn.checkAnimFrame)
-    cancelRequestAnimFrame(pJSDom[0].pJS.fn.drawAnimFrame)
-    pJSDom[0].pJS.fn.particlesEmpty()
-    pJSDom[0].pJS.fn.canvasClear()
-  })
-)
-
-obsParticles.observe(JSparticleEl) */
+    }
+  },
+  retina_detect: true
+})
 
 activePageHighlight()
 animateOnScroll()
