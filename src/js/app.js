011cb6c040bc444b166c1bea677a5e961b0fb512
Roman <romanchebotarenko@gmail.com>
2022-09-25 04:50:33 +0300

    111

--- src/js/app.js
+++ src/js/app.js
@@ -10,228 +10,116 @@ import { animateOnScroll } from './modules/animate-on-scroll.js'
 import { search } from './modules/search.js'
 import { md, lg, sm } from './modules/breakpoints.js'
 
-particlesJS(
-  'banner__particles',
-  {
-    particles: {
-      number: {
-        value: 15,
-        density: {
-          enable: true,
-          value_area: 800
-        }
-      },
-      color: {
-        value: '#348acc'
-      },
-      shape: {
-        type: 'circle',
-        stroke: {
-          width: 0,
-          color: '#000000'
-        },
-        polygon: {
-          nb_sides: 3
-        },
-        image: {
-          src: 'img/github.svg',
-          width: 100,
-          height: 100
-        }
-      },
-      opacity: {
-        value: 0.6,
-        random: false,
-        anim: {
-          enable: false,
-          speed: 1,
-          opacity_min: 0.1,
-          sync: false
-        }
-      },
-      size: {
-        value: 3,
-        random: true,
-        anim: {
-          enable: false,
-          speed: 40,
-          size_min: 0.1,
-          sync: false
-        }
-      },
-      line_linked: {
-        enable: true,
-        distance: 300,
-        color: '#348acc',
-        opacity: 0.9,
-        width: 1
-      },
-      move: {
-        enable: true,
-        speed: 0.5,
-        direction: 'bottom',
-        random: false,
-        straight: false,
-        out_mode: 'out',
-        bounce: false,
-        attract: {
-          enable: false,
-          rotateX: 600,
-          rotateY: 1200
-        }
-      }
-    },
-    interactivity: {
-      detect_on: 'canvas',
-      events: {
-        onhover: {
-          enable: false,
-          mode: 'repulse'
-        },
-        onclick: {
-          enable: false,
-          mode: 'push'
-        },
-        resize: true
-      },
-      modes: {
-        grab: {
-          distance: 400,
-          line_linked: {
-            opacity: 0.9
-          }
-        },
-        bubble: {
-          distance: 400,
-          size: 40,
-          duration: 2,
-          opacity: 8,
-          speed: 3
-        },
-        repulse: {
-          distance: 200,
-          duration: 0.4
-        },
-        push: {
-          particles_nb: 4
-        },
-        remove: {
-          particles_nb: 2
-        }
-      }
-    },
-    retina_detect: true
-  } /* {
+particlesJS('banner__particles', {
   particles: {
     number: {
-      value: 8,
+      value: 15,
       density: {
         enable: true,
-        value_area: 400
-      }
+        value_area: 800,
+      },
     },
     color: {
-      value: '#348ACC'
+      value: '#348acc',
     },
     shape: {
       type: 'circle',
       stroke: {
         width: 0,
-        color: '#348ACC'
+        color: '#000000',
       },
       polygon: {
-        nb_sides: 5
+        nb_sides: 3,
       },
       image: {
-        src: '',
+        src: 'img/github.svg',
         width: 100,
-        height: 100
-      }
+        height: 100,
+      },
     },
     opacity: {
-      value: 1,
+      value: 0.6,
       random: false,
       anim: {
         enable: false,
         speed: 1,
         opacity_min: 0.1,
-        sync: false
-      }
+        sync: false,
+      },
     },
     size: {
-      value: 5,
+      value: 3,
       random: true,
       anim: {
-        enable: true,
-        speed: 1,
+        enable: false,
+        speed: 40,
         size_min: 0.1,
-        sync: false
-      }
+        sync: false,
+      },
     },
     line_linked: {
       enable: true,
-      distance: 250,
-      color: '#348ACC',
-      opacity: 1,
-      width: 1
+      distance: 300,
+      color: '#348acc',
+      opacity: 0.9,
+      width: 1,
     },
     move: {
       enable: true,
-      speed: 3,
-      direction: 'none',
-      random: true,
+      speed: 0.5,
+      direction: 'bottom',
+      random: false,
       straight: false,
-      out_mode: 'bounce',
+      out_mode: 'out',
       bounce: false,
       attract: {
-        enable: true,
+        enable: false,
         rotateX: 600,
-        rotateY: 1200
-      }
-    }
+        rotateY: 1200,
+      },
+    },
   },
   interactivity: {
     detect_on: 'canvas',
     events: {
       onhover: {
-        enable: true,
-        mode: 'repulse'
+        enable: false,
+        mode: 'repulse',
       },
       onclick: {
         enable: false,
-        mode: 'push'
+        mode: 'push',
       },
-      resize: true
+      resize: true,
     },
     modes: {
       grab: {
-        distance: 300,
+        distance: 400,
         line_linked: {
-          opacity: 1
-        }
+          opacity: 0.9,
+        },
       },
       bubble: {
-        distance: 300,
-        size: 9,
-        duration: 10,
-        opacity: 1,
-        speed: 1
+        distance: 400,
+        size: 40,
+        duration: 2,
+        opacity: 8,
+        speed: 3,
       },
       repulse: {
-        distance: 400,
-        duration: 0.1
+        distance: 200,
+        duration: 0.4,
       },
       push: {
-        particles_nb: 4
+        particles_nb: 4,
       },
       remove: {
-        particles_nb: 2
-      }
-    }
+        particles_nb: 2,
+      },
+    },
   },
-  retina_detect: true
-} */
-)
+  retina_detect: true,
+})
 
 activePageHighlight()
 animateOnScroll()
@@ -239,7 +127,7 @@ animateOnScroll()
 fixedHeader(lg)
 
 hamburger({
-  notification: '[data-notification] button'
+  notification: '[data-notification] button',
 })
 
 dropdown()
--- src/js/modules/hamburger.js
+++ src/js/modules/hamburger.js
@@ -11,20 +11,19 @@ const defaultOptions = {
   smartFullscreen: false,
   scrollBlock: false,
   stopTransition: false,
-};
+}
 
 /*  ---------------------- RUN  -------------------------- */
 
 export function hamburger(userOptions) {
-  let options = defaultOptions;
-  let mainElement;
+  let options = defaultOptions
+  let mainElement
 
   userOptions && 'mainElement' in userOptions
     ? (mainElement = document.querySelector(userOptions.mainElement))
-    : (mainElement = document.querySelector(defaultOptions.mainElement));
-
-  if (userOptions) options = { ...defaultOptions, ...userOptions };
-
+    : (mainElement = document.querySelector(defaultOptions.mainElement))
+    
+  if (userOptions) options = { ...defaultOptions, ...userOptions }
   // destructor
   let {
     notification,
@@ -38,168 +37,151 @@ export function hamburger(userOptions) {
     alwaysFullscreen,
     scrollBlock,
     stopTransition,
-  } = options;
+  } = options
 
-  const headerHeight = document.querySelector('header').scrollHeight;
+  const headerHeight = document.querySelector('header').scrollHeight
   const wrapAll = (target, wrapper = document.createElement('div')) => {
-    [...target.childNodes].forEach((child) => wrapper.appendChild(child));
-    target.appendChild(wrapper);
-    return wrapper;
-  };
+    ;[...target.childNodes].forEach(child => wrapper.appendChild(child))
+    target.appendChild(wrapper)
+    return wrapper
+  }
 
-  const content = wrapAll(mainElement);
-  content.style.overflowY = 'auto';
-  
+  const content = wrapAll(mainElement)
+  content.style.overflowY = 'auto'
 
-  mainElement.id = hamburgerId;
-  toggler = document.querySelector(toggler);
-  toggler.type = 'button';
-  toggler.setAttribute('aria-expanded', 'false');
-  toggler.setAttribute('aria-hasPopup', 'true');
-  toggler.setAttribute('aria-label', `Toggle ${hamburgerId}`);
-  toggler.setAttribute('aria-controls', hamburgerId);
+  mainElement.id = hamburgerId
+  toggler = document.querySelector(toggler)
+  toggler.type = 'button'
+  toggler.setAttribute('aria-expanded', 'false')
+  toggler.setAttribute('aria-hasPopup', 'true')
+  toggler.setAttribute('aria-label', `Toggle ${hamburgerId}`)
+  toggler.setAttribute('aria-controls', hamburgerId)
 
   const firstFocusableEl = mainElement.querySelector(
     'button, [href]:not(use), input, select, textarea, [tabindex]:not([tabindex="-1"])'
-  );
+  )
 
-  let contentHasTransition = getComputedStyle(mainElement).getPropertyValue(
-    'transition-duration'
-  );
+  let contentHasTransition = getComputedStyle(mainElement).getPropertyValue('transition-duration')
 
-  if (contentHasTransition === '0s') contentHasTransition = false;
+  if (contentHasTransition === '0s') contentHasTransition = false
 
   if (notification) {
-    notification = document.querySelector(notification);
+    notification = document.querySelector(notification)
   }
 
-  if (stopTransition) mainElement.classList.add('stop-transition');
+  if (stopTransition) mainElement.classList.add('stop-transition')
 
   // add error check here for missing elements - mainElement, openBtn, closeBtn, notification, content
 
-  toggler.addEventListener('click', open);
-  toggler.addEventListener('keydown', keyboardNavigation);
+  toggler.addEventListener('click', open)
+  toggler.addEventListener('keydown', keyboardNavigation)
 
   // adjust height on orientation change
   window.matchMedia('(orientation: landscape)').onchange = () => {
-    if (mainElement.classList.contains('active')) calcContentHeight();
-  };
+    if (mainElement.classList.contains('active')) calcContentHeight()
+  }
 
   // media
   if (breakpoint) {
-    const watchBreakpoint = window.matchMedia(`(max-width: ${breakpoint}px)`);
+    const watchBreakpoint = window.matchMedia(`(max-width: ${breakpoint}px)`)
 
     if (!watchBreakpoint.matches) {
-      content.style.overflowY = null;
+      content.style.overflowY = null
     }
 
-    watchBreakpoint.onchange = (e) => {
-      e.matches
-        ? (content.style.overflowY = 'auto')
-        : (content.style.overflowY = null);
+    watchBreakpoint.onchange = e => {
+      e.matches ? (content.style.overflowY = 'auto') : (content.style.overflowY = null)
 
-      if (mainElement.classList.contains('active') && !e.matches) close();
-    };
+      if (mainElement.classList.contains('active') && !e.matches) close()
+    }
   }
 
   /* ====================   FUNCTIONS   ==================== */
 
   function open() {
-    if (mainElement.classList.contains('active')) return close();
-    mainElement.classList.add('active');
-    mainElement.setAttribute('aria-modal', 'true');
-    mainElement.setAttribute('role', 'dialog');
+    if (mainElement.classList.contains('active')) return close()
+    mainElement.classList.add('active')
+    mainElement.setAttribute('aria-modal', 'true')
+    mainElement.setAttribute('role', 'dialog')
 
-    toggler.classList.add('active');
-    toggler.setAttribute('aria-expanded', 'true');
+    toggler.classList.add('active')
+    toggler.setAttribute('aria-expanded', 'true')
 
-    document.body.style.overflow = 'hidden';
-    document.addEventListener('keydown', closeWithEsc);
+    document.body.style.overflow = 'hidden'
+    document.addEventListener('keydown', closeWithEsc)
 
-    calcContentHeight();
-    window.visualViewport.addEventListener('resize', calcContentHeight); // mobile browser header fix
+    calcContentHeight()
+    window.visualViewport.addEventListener('resize', calcContentHeight) // mobile browser header fix
 
-    if (stopTransition) mainElement.classList.remove('stop-transition');
-
-    if (notification) notification.click();
-
-    if (backdrop) addBackdrop(backdrop);
-
-    if (closeOnBackdropClick) backdrop.addEventListener('click', close);
-
-    if (scrollBlock) document.body.style.overflow = 'hidden';
+    if (stopTransition) mainElement.classList.remove('stop-transition')
+    if (notification) notification.click()
+    if (backdrop) addBackdrop(backdrop)
+    if (closeOnBackdropClick) backdrop.addEventListener('click', close)
+    if (scrollBlock) document.body.style.overflow = 'hidden'
   }
 
   function close() {
-    mainElement.classList.remove('active');
-    mainElement.style.height = null;
-    mainElement.removeAttribute('aria-modal');
-    mainElement.removeAttribute('role');
-    toggler.classList.remove('active');
-    toggler.setAttribute('aria-expanded', 'false');
+    mainElement.classList.remove('active')
+    mainElement.style.height = null
+    mainElement.removeAttribute('aria-modal')
+    mainElement.removeAttribute('role')
+    toggler.classList.remove('active')
+    toggler.setAttribute('aria-expanded', 'false')
 
-    document.body.style.overflow = null;
-    document.removeEventListener('keyup', closeWithEsc);
+    document.body.style.overflow = null
+    document.removeEventListener('keyup', closeWithEsc)
 
-    window.visualViewport.removeEventListener('resize', calcContentHeight);
-    content.style.maxHeight = null;
+    window.visualViewport.removeEventListener('resize', calcContentHeight)
+    content.style.maxHeight = null
 
-    if (backdrop || smartBackdrop) removeBackdrop();
+    if (backdrop || smartBackdrop) removeBackdrop()
 
     if (stopTransition) {
-      mainElement.addEventListener(
-        'transitionend',
-        () => mainElement.classList.add('stop-transition'),
-        { once: true }
-      );
+      mainElement.addEventListener('transitionend', () => mainElement.classList.add('stop-transition'), { once: true })
     }
   }
 
   function keyboardNavigation() {
-    if (!contentHasTransition) firstFocusableEl.focus();
+    if (!contentHasTransition) firstFocusableEl.focus()
 
     if (contentHasTransition)
-      mainElement.addEventListener(
-        'transitionend',
-        () => firstFocusableEl.focus(),
-        {
-          once: true,
-        }
-      );
+      mainElement.addEventListener('transitionend', () => firstFocusableEl.focus(), {
+        once: true,
+      })
 
-    mainElement.addEventListener('focusout', focusOut);
+    mainElement.addEventListener('focusout', focusOut)
   }
 
   function focusOut() {
     setTimeout(() => {
       if (!mainElement.contains(document.activeElement)) {
-        mainElement.removeEventListener('focusout', focusOut);
-        close();
-        toggler.focus();
+        mainElement.removeEventListener('focusout', focusOut)
+        close()
+        toggler.focus()
       }
-    }, 25);
+    }, 25)
   }
 
   function closeWithEsc(e) {
     if (e.key === 'Escape' || e.key === 'Esc' || e.code === 27) {
-      close();
+      close()
     }
   }
 
   function calcContentHeight() {
-    content.style.maxHeight = `${window.innerHeight - headerHeight}px`;
+    content.style.maxHeight = `${window.innerHeight - headerHeight}px`
   }
 
   function addBackdrop(backdropClass) {
-    const createBackdrop = document.createElement('div');
-    createBackdrop.classList.add('nav-backdrop');
-    document.querySelector('.page-wrapper').after(createBackdrop);
-    backdrop = document.querySelector('.nav-backdrop');
-    backdrop.classList.add(backdropClass);
+    const createBackdrop = document.createElement('div')
+    createBackdrop.classList.add('nav-backdrop')
+    document.querySelector('.page-wrapper').after(createBackdrop)
+    backdrop = document.querySelector('.nav-backdrop')
+    backdrop.classList.add(backdropClass)
   }
 
   function removeBackdrop() {
-    backdrop.remove();
+    backdrop.remove()
   }
 
   /* function removeLater() {
