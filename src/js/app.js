cb132ad60b995237770a6108a1b9a8dcb3f2c75c
Roman <romanchebotarenko@gmail.com>
2022-09-28 04:54:45 +0300

    hamburger rework

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,13 @@
 import './modules/swiper.js'
 import 'particles.js'
-import { notification } from './modules/notification.js'
+//import { notification } from './modules/notification.js'
 import { fixedHeader, headroomHeader } from './modules/headers.js'
-import { accordion } from './modules/accordion.js'
+//import { accordion } from './modules/accordion.js'
 import { activePageHighlight } from './modules/active-page-highlight.js'
 import { dropdown } from './modules/dropdown.js'
 import { hamburger } from './modules/hamburger.js'
 import { animateOnScroll } from './modules/animate-on-scroll.js'
-import { search } from './modules/search.js'
+//import { search } from './modules/search.js'
 import { md, lg, sm } from './modules/breakpoints.js'
 
 particlesJS('banner__particles', {
@@ -123,11 +123,8 @@ particlesJS('banner__particles', {
 
 activePageHighlight()
 animateOnScroll()
+fixedHeader()
 
-fixedHeader(lg)
-
-hamburger({
-  notification: '[data-notification] button',
-})
+hamburger()
 
 dropdown()
--- src/js/modules/hamburger.js
+++ src/js/modules/hamburger.js
@@ -1,16 +1,9 @@
 const defaultOptions = {
   mainElement: '[data-hamburger]',
-  toggler: '[data-hamburger-btn]',
+  togglerOpen: '[data-hamburger-btn="open"]',
+  togglerClose: '[data-hamburger-btn="close"]',
   hamburgerId: 'navigation',
-  notification: false,
-  breakpoint: 992,
-  backdrop: false,
-  smartBackdrop: false,
-  closeOnBackdropClick: false,
-  alwaysFullscreen: false,
-  smartFullscreen: false,
-  scrollBlock: false,
-  stopTransition: false,
+  breakpoint: 1200,
 }
 
 /*  ---------------------- RUN  -------------------------- */
@@ -22,132 +15,85 @@ export function hamburger(userOptions) {
   userOptions && 'mainElement' in userOptions
     ? (mainElement = document.querySelector(userOptions.mainElement))
     : (mainElement = document.querySelector(defaultOptions.mainElement))
-    
+
   if (userOptions) options = { ...defaultOptions, ...userOptions }
   // destructor
-  let {
-    notification,
-    toggler,
-    hamburgerId,
-    breakpoint,
-    backdrop,
-    smartBackdrop,
-    closeOnBackdropClick,
-    smartFullscreen,
-    alwaysFullscreen,
-    scrollBlock,
-    stopTransition,
-  } = options
-
-  const headerHeight = document.querySelector('header').scrollHeight
-  const wrapAll = (target, wrapper = document.createElement('div')) => {
-    ;[...target.childNodes].forEach(child => wrapper.appendChild(child))
-    target.appendChild(wrapper)
-    return wrapper
-  }
-
-  const content = wrapAll(mainElement)
-  content.style.overflowY = 'auto'
+  let { togglerOpen, togglerClose, hamburgerId, breakpoint } = options
 
   mainElement.id = hamburgerId
-  toggler = document.querySelector(toggler)
-  toggler.type = 'button'
-  toggler.setAttribute('aria-expanded', 'false')
-  toggler.setAttribute('aria-hasPopup', 'true')
-  toggler.setAttribute('aria-label', `Toggle ${hamburgerId}`)
-  toggler.setAttribute('aria-controls', hamburgerId)
+  mainElement.after(document.createElement('div'))
+  const backdrop = document.querySelector('.backdrop')
+  backdrop.classList.add('backdrop')
 
-  const firstFocusableEl = mainElement.querySelector(
+  togglerOpen = document.querySelector(togglerOpen)
+  togglerClose = document.querySelector(togglerClose)
+
+  //toggler.setAttribute('aria-expanded', 'false')
+  //toggler.setAttribute('aria-hasPopup', 'true')
+  //toggler.setAttribute('aria-label', `Toggle ${hamburgerId}`)
+  //toggler.setAttribute('aria-controls', hamburgerId)
+
+  /* const firstFocusableEl = mainElement.querySelector(
     'button, [href]:not(use), input, select, textarea, [tabindex]:not([tabindex="-1"])'
-  )
+  ) */
 
-  let contentHasTransition = getComputedStyle(mainElement).getPropertyValue('transition-duration')
+  //let contentHasTransition = getComputedStyle(mainElement).getPropertyValue('transition-duration')
+  //if (contentHasTransition === '0s') contentHasTransition = false
 
-  if (contentHasTransition === '0s') contentHasTransition = false
+  mainElement.classList.add('stop-transition')
 
-  if (notification) {
-    notification = document.querySelector(notification)
-  }
-
-  if (stopTransition) mainElement.classList.add('stop-transition')
-
-  // add error check here for missing elements - mainElement, openBtn, closeBtn, notification, content
-
-  toggler.addEventListener('click', open)
-  toggler.addEventListener('keydown', keyboardNavigation)
-
-  // adjust height on orientation change
-  window.matchMedia('(orientation: landscape)').onchange = () => {
-    if (mainElement.classList.contains('active')) calcContentHeight()
-  }
+  togglerOpen.addEventListener('click', open)
+  togglerClose.addEventListener('click', close)
+  backdrop.addEventListener('click', close)
+  //toggler.addEventListener('keydown', keyboardNavigation)
 
   // media
   if (breakpoint) {
     const watchBreakpoint = window.matchMedia(`(max-width: ${breakpoint}px)`)
-
-    if (!watchBreakpoint.matches) {
-      content.style.overflowY = null
-    }
-
     watchBreakpoint.onchange = e => {
-      e.matches ? (content.style.overflowY = 'auto') : (content.style.overflowY = null)
-
-      if (mainElement.classList.contains('active') && !e.matches) close()
+      if (mainElement.classList.contains('active') && !e.matches) {
+        mainElement.classList.add('stop-transition')
+        // block backdrop transition
+        close()
+      }
     }
   }
 
   /* ====================   FUNCTIONS   ==================== */
 
   function open() {
-    if (mainElement.classList.contains('active')) return close()
     mainElement.classList.add('active')
     mainElement.setAttribute('aria-modal', 'true')
     mainElement.setAttribute('role', 'dialog')
-
-    toggler.classList.add('active')
-    toggler.setAttribute('aria-expanded', 'true')
-
+    //toggler.setAttribute('aria-expanded', 'true')
     document.body.style.overflow = 'hidden'
-    document.addEventListener('keydown', closeWithEsc)
+    //document.addEventListener('keydown', closeWithEsc)
 
-    calcContentHeight()
-    window.visualViewport.addEventListener('resize', calcContentHeight) // mobile browser header fix
+    mainElement.classList.remove('stop-transition')
 
-    if (stopTransition) mainElement.classList.remove('stop-transition')
-    if (notification) notification.click()
-    if (backdrop) addBackdrop(backdrop)
-    if (closeOnBackdropClick) backdrop.addEventListener('click', close)
-    if (scrollBlock) document.body.style.overflow = 'hidden'
+    //addBackdrop(backdrop)
+    //backdrop.addEventListener('click', close)
+
+    backdrop.classList.add('active')
   }
 
   function close() {
     mainElement.classList.remove('active')
-    mainElement.style.height = null
     mainElement.removeAttribute('aria-modal')
     mainElement.removeAttribute('role')
-    toggler.classList.remove('active')
-    toggler.setAttribute('aria-expanded', 'false')
-
+    //toggler.classList.remove('active')
+    //toggler.setAttribute('aria-expanded', 'false')
+    backdrop.classList.remove('active')
     document.body.style.overflow = null
-    document.removeEventListener('keyup', closeWithEsc)
-
-    window.visualViewport.removeEventListener('resize', calcContentHeight)
-    content.style.maxHeight = null
-
-    if (backdrop || smartBackdrop) removeBackdrop()
-
-    if (stopTransition) {
-      mainElement.addEventListener('transitionend', () => mainElement.classList.add('stop-transition'), { once: true })
-    }
+    //document.removeEventListener('keyup', closeWithEsc)
+    //removeBackdrop()
+    mainElement.addEventListener('transitionend', () => mainElement.classList.add('stop-transition'), { once: true })
   }
 
   function keyboardNavigation() {
-    if (!contentHasTransition) firstFocusableEl.focus()
-
-    if (contentHasTransition)
-      mainElement.addEventListener('transitionend', () => firstFocusableEl.focus(), {
-        once: true,
-      })
+    mainElement.addEventListener('transitionend', () => firstFocusableEl.focus(), {
+      once: true,
+    })
 
     mainElement.addEventListener('focusout', focusOut)
   }
@@ -167,65 +113,4 @@ export function hamburger(userOptions) {
       close()
     }
   }
-
-  function calcContentHeight() {
-    content.style.maxHeight = `${window.innerHeight - headerHeight}px`
-  }
-
-  function addBackdrop(backdropClass) {
-    const createBackdrop = document.createElement('div')
-    createBackdrop.classList.add('nav-backdrop')
-    document.querySelector('.page-wrapper').after(createBackdrop)
-    backdrop = document.querySelector('.nav-backdrop')
-    backdrop.classList.add(backdropClass)
-  }
-
-  function removeBackdrop() {
-    backdrop.remove()
-  }
-
-  /* function removeLater() {
-     (alwaysFullscreen) {
-      mainElement.style.height = availableViewportHeight + 'px';
-      document.body.style.overflow = 'hidden';
-      //console.log('USING ALL AVAILABLE VIEWPORT HEIGHT, BLOCKING BODY SCROLL');
-      return;
-    }
-
-    if (availableViewportHeight - contentHeight < 0) {
-      mainElement.style.height = availableViewportHeight + 'px';
-      document.body.style.overflow = 'hidden';
-         console.log(
-        'CONTENT DOES NOT FIT VIEWPORT => ADDING HEIGHT, MAKING IT SCROLLABLE, BLOCKING BODY SCROLL'
-      ); 
-      return;
-    }
-
-    if (availableViewportHeight - contentHeight < availableViewportHeight / 2) {
-      mainElement.style.height = null;
-      document.body.style.overflow = 'hidden';
-
-      if (smartFullscreen)
-        mainElement.style.height = availableViewportHeight + 'px';
-
-      if (smartBackdrop) {
-        removeBackdrop();
-        addBackdrop(smartBackdrop);
-        backdropEl.addEventListener('click', close);
-      }
-
-       console.log(
-        'NAVIGATION CONTENT HAS NOT MUCH FREE SPACE FOR SCROLL, ACTIVATING BACKDROP/FULLSCREEN AND LOCKING BODY SCROLL'
-      ); 
-
-      return;
-    }
-
-    mainElement.style.height = null;
-    if (!scrollBlock) document.body.style.overflow = null;
-
-    console.log(
-      'NAVIGATION CONTENT HAS ENOUGH VIEWPORT SPACE, REMOVING HEIGHT, ALLOWING BODY SCROLL'
-    ); 
-  } */
 }
